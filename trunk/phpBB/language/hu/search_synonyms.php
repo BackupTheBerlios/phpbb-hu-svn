9d451b12aacd2ce24f70405da01f21ef9e463617
fberci <fberci@c395b634-4022-4dee-a7d9-fccbfec04023>
2008-01-26 22:44:17 +0000

    Kezdeti feltöltés: phpBB 3.0.0 (magyar)

---
+++ trunk/phpBB/language/hu/search_synonyms.php
@@ -0,0 +1,17 @@
+<?php
+/**
+*
+* search_synonyms [English]
+*
+* @package language
+* @version $Id$
+* @copyright (c) 2007 �Magyar phpBB K�z�ss�g ford�t�k�
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* Original copyright: (c) 2005 phpBB Group
+
+*
+*/
+
+$synonyms = array(
+);
+?>
